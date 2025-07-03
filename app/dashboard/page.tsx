0b2e156b5e2e673e15b9ea178c82d3599d95781c
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-04 01:04:00 +0700

    add: dashboard section

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Selamat Datang di Dashboard</h2>
+      <p>Pilih menu di sidebar untuk mulai.</p>
+    </div>
+  );
+}
