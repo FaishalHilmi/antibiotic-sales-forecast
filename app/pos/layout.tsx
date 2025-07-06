0c718ed66a08d4b81aaed89480c9a33834558e32
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-06 23:12:51 +0700

    chore: update NavbarPos in layout

--- app/pos/layout.tsx
+++ app/pos/layout.tsx
@@ -5,7 +5,7 @@ export default function POSLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col text-black">
       <NavbarPos />
-      <main className="flex-1 py-4 px-4 sm:px-20">{children}</main>
+      <main className="flex-1 py-8 px-4 sm:px-20">{children}</main>
     </div>
   );
 }
