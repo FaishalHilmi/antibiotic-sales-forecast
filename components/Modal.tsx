1e590d0d094510338a56dcddfc342e01b75cf924
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-07 12:04:30 +0700

    add: Modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { X } from "lucide-react";
+import { ModalProps } from "@/types/modal";
+
+export default function Modal({
+  isOpen,
+  onClose,
+  title,
+  children,
+}: ModalProps) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 px-4">
+      <div className="bg-white p-6 rounded-xl w-full max-w-lg max-h-[90vh] overflow-y-auto relative animate-fade-in">
+        <button
+          onClick={onClose}
+          className="absolute top-3 right-3 text-gray-500 hover:text-black"
+        >
+          <X />
+        </button>
+
+        <h2 className="font-bold text-xl mb-4 text-center">{title}</h2>
+
+        {children}
+      </div>
+
+      <style jsx>{`
+        .animate-fade-in {
+          animation: fadeIn 0.3s ease-out;
+        }
+        @keyframes fadeIn {
+          from {
+            opacity: 0;
+            transform: scale(0.95);
+          }
+          to {
+            opacity: 1;
+            transform: scale(1);
+          }
+        }
+      `}</style>
+    </div>
+  );
+}
