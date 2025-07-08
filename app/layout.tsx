a87f7194e3d451583f1206477c5bf1630512027b
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-08 23:02:28 +0700

    chore: add medium font weight

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  weight: ["400", "600", "700"],
+  weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
   display: "swap",
 });
