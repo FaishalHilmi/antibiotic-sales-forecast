fdafc0757af208805a6cbc4b2f0d96e8f7121448
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-08 23:52:09 +0700

    chore: to add Navlink

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,52 @@
 "use client";
 import Link from "next/link";
 import { useSidebar } from "@/context/SidebarContext";
-import { Home, LineChart } from "lucide-react";
+import { usePathname } from "next/navigation";
+import {
+  ClipboardList,
+  FileText,
+  Home,
+  LineChart,
+  Pill,
+  Users2,
+} from "lucide-react";
 
 export default function Sidebar() {
   const { isExpanded, isMobileOpen, toggleMobileSidebar } = useSidebar();
+  const pathname = usePathname();
+
+  const navLinkData = [
+    {
+      name: "Dashboard",
+      route: "/dashboard",
+      icon: <Home className="w-5 h-5" />,
+    },
+    {
+      name: "Kelola Obat",
+      route: "/kelola-obat",
+      icon: <Pill className="w-5 h-5" />, 
+    },
+    {
+      name: "Riwayat Penjualan",
+      route: "/kelola-penjualan",
+      icon: <FileText className="w-5 h-5" />, 
+    },
+    {
+      name: "Peramalan",
+      route: "/peramalan",
+      icon: <LineChart className="w-5 h-5" />,
+    },
+    {
+      name: "Rekap Penjualan",
+      route: "/rekap-penjualan",
+      icon: <ClipboardList className="w-5 h-5" />,
+    },
+    {
+      name: "Kelola Akun Kasir",
+      route: "/kelola-akun-kasir",
+      icon: <Users2 className="w-5 h-5" />,
+    },
+  ];
 
   return (
     <>
@@ -41,22 +83,24 @@ export default function Sidebar() {
             !isExpanded && "items-center"
           }`}
         >
-          <Link
-            href="/dashboard"
-            className="flex items-center p-2 rounded hover:bg-gray-200"
-            onClick={toggleMobileSidebar}
-          >
-            <Home className="w-5 h-5" />
-            {isExpanded && <span className="ml-3">Home</span>}
-          </Link>
-          <Link
-            href="/dashboard/forecast"
-            className="flex items-center p-2 rounded hover:bg-gray-200"
-            onClick={toggleMobileSidebar}
-          >
-            <LineChart className="w-5 h-5" />
-            {isExpanded && <span className="ml-3">Peramalan</span>}
-          </Link>
+          {navLinkData.map((navLink, i) => (
+            <Link
+              key={i}
+              href={navLink.route}
+              className={`flex items-center p-2 rounded-md transition-colors duration-200
+              ${
+                pathname === navLink.route
+                  ? "bg-primary text-white"
+                  : "hover:bg-gray-200 text-gray-700"
+              }
+              ${!isExpanded && "justify-center"}
+            `}
+              onClick={toggleMobileSidebar}
+            >
+              {navLink.icon}
+              {isExpanded && <span className="ml-3">{navLink.name}</span>}
+            </Link>
+          ))}
         </nav>
       </div>
     </>
