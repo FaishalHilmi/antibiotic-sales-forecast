698e475bfa290c1291b8674bcb863f36350f8d56
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-10 00:25:53 +0700

    add: dummy data transaction for dashboard

---
+++ data/transaction.ts
@@ -0,0 +1,32 @@
+export const transactions = [
+  {
+    id: "TRX-00121",
+    date: "Sen, 8 Juli 2025, 09:32",
+    cashier: "Admin 1",
+    total: 24000,
+  },
+  {
+    id: "TRX-00120",
+    date: "Sen, 8 Juli 2025, 09:15",
+    cashier: "Admin 2",
+    total: 56000,
+  },
+  {
+    id: "TRX-00119",
+    date: "Min, 7 Juli 2025, 17:44",
+    cashier: "Admin 1",
+    total: 33000,
+  },
+  {
+    id: "TRX-00118",
+    date: "Min, 7 Juli 2025, 16:12",
+    cashier: "Admin 2",
+    total: 75000,
+  },
+  {
+    id: "TRX-00117",
+    date: "Min, 7 Juli 2025, 15:34",
+    cashier: "Admin 3",
+    total: 18000,
+  },
+];
