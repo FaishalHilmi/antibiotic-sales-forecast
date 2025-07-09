733d1191f2bc78873436b7c5125fb9679d0fb7b6
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-10 00:28:54 +0700

    chore: update to import data navlink from sidebarNavLink

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,52 +1,11 @@
-"use client";
 import Link from "next/link";
 import { useSidebar } from "@/context/SidebarContext";
 import { usePathname } from "next/navigation";
-import {
-  ClipboardList,
-  FileText,
-  Home,
-  LineChart,
-  Pill,
-  Users2,
-} from "lucide-react";
+import { navLinkData } from "@/data/sidebarNavLink";
 
 export default function Sidebar() {
   const { isExpanded, isMobileOpen, toggleMobileSidebar } = useSidebar();
   const pathname = usePathname();
-
-  const navLinkData = [
-    {
-      name: "Dashboard",
-      route: "/dashboard",
-      icon: <Home className="w-5 h-5" />,
-    },
-    {
-      name: "Kelola Obat",
-      route: "/kelola-obat",
-      icon: <Pill className="w-5 h-5" />, 
-    },
-    {
-      name: "Riwayat Penjualan",
-      route: "/kelola-penjualan",
-      icon: <FileText className="w-5 h-5" />, 
-    },
-    {
-      name: "Peramalan",
-      route: "/peramalan",
-      icon: <LineChart className="w-5 h-5" />,
-    },
-    {
-      name: "Rekap Penjualan",
-      route: "/rekap-penjualan",
-      icon: <ClipboardList className="w-5 h-5" />,
-    },
-    {
-      name: "Kelola Akun Kasir",
-      route: "/kelola-akun-kasir",
-      icon: <Users2 className="w-5 h-5" />,
-    },
-  ];
 
   return (
     <>
