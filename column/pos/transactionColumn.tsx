fab0c670c3ded4b234e39e03eb8b43369cb9a2b8
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-11 00:29:09 +0700

    chore: change value on Link to be id

--- column/pos/transactionColumn.tsx
+++ column/pos/transactionColumn.tsx
@@ -26,13 +26,13 @@ export const transactionColumn = [
     cell: (row: any) => (
       <div className="flex flex-col md:flex-row gap-1 md:gap-2 py-2 md:py-0">
         <Link
-          href={`/history/${row.transactionId}`}
+          href={`/history/${row.id}`}
           className="bg-blue-600 text-white text-xs px-3 py-2 rounded-lg"
         >
           Detail
         </Link>
         <Link
-          href={`/history/delete/${row.transactionId}`}
+          href={`/history/delete/${row.id}`}
           className="bg-red-500 text-white text-xs px-3 py-2 rounded-lg"
         >
           Hapus
