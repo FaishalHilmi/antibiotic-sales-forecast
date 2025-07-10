c3be80037c5ebe4001b2cc620d3e4035b0a79ffd
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-11 00:29:50 +0700

    add: Search bar component

---
+++ components/SearchBar.tsx
@@ -0,0 +1,18 @@
+import { SearchBarProps } from "@/types/searchbar";
+
+export default function SearchBar({
+  value,
+  onChange,
+  placeholder = "Cari...",
+  className = "",
+}: SearchBarProps) {
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      className={`border border-gray-200 rounded-xl px-3 py-2 mb-4 text-sm outline-gray-300 ${className}`}
+    />
+  );
+}
