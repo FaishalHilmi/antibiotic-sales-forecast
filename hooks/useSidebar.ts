e2a69cf64b7f871573bd85f8b01f5cd237bd6f3f
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-10 17:40:51 +0700

    add: useSidebar custom hook

---
+++ hooks/useSidebar.ts
@@ -0,0 +1,9 @@
+import { SidebarContext } from "@/context/SidebarContext";
+import { useContext } from "react";
+
+export const useSidebar = () => {
+  const context = useContext(SidebarContext);
+  if (!context)
+    throw new Error("useSidebar must be used within SidebarProvider");
+  return context;
+};
