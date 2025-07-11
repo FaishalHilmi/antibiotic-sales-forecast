f675f4874c6a8c6bfd0c664051593cddfdda50b4
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-12 00:19:04 +0700

    add: Create InputText component for user input handling

---
+++ components/InputText.tsx
@@ -0,0 +1,23 @@
+import { InputProps } from "@/types/input";
+
+export default function InputText({
+  label,
+  type = "text",
+  id,
+  value,
+  onChange,
+}: InputProps) {
+  return (
+    <div className="flex flex-col gap-1">
+      <label htmlFor={id}>{label}</label>
+      <input
+        type={type}
+        id={id}
+        value={value ?? ""}
+        onChange={onChange}
+        className="border border-gray-300 rounded-lg py-1.5 px-3 outline-primary"
+        required
+      />
+    </div>
+  );
+}
