c2fe1b3f4b5d38124dfd7a53b783132d3ec17937
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-12 00:17:49 +0700

    add: Implement SelectInput component for dynamic selection

---
+++ components/SelectInput.tsx
@@ -0,0 +1,29 @@
+import { SelectInputProps } from "@/types/input";
+
+export default function SelectInput({
+  label,
+  id,
+  options,
+  value,
+  onChange,
+}: SelectInputProps) {
+  return (
+    <div className="flex flex-col gap-1">
+      <label htmlFor={id}>{label}</label>
+      <select
+        id={id}
+        className="border border-gray-300 rounded-lg py-1.5 px-3 outline-primary"
+        value={value}
+        onChange={onChange}
+        required
+      >
+        <option value="">Pilih {label.toLowerCase()}</option>
+        {options.map((option, i) => (
+          <option key={i} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
