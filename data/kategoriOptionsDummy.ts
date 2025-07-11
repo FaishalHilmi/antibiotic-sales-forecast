1df826695c98d61e0d9602c16648a8d66b96ae95
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-12 00:17:22 +0700

    add: Create kategoriOptionsDummy data for antibiotic categories

---
+++ data/kategoriOptionsDummy.ts
@@ -0,0 +1,6 @@
+export const kategoriOptionsDummy = [
+  { value: "Antibiotik", label: "Antibiotik" },
+  { value: "Analgesik", label: "Analgesik" },
+  { value: "Antipiretik", label: "Antipiretik" },
+  { value: "Antijamur", label: "Antijamur" },
+];
