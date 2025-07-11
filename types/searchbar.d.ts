3faba4f4291ddabc548ee7f85d372a8b393d67d2
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-12 00:23:43 +0700

    add: Define SearchBarProps interface for search bar component

---
+++ types/searchbar.d.ts
@@ -0,0 +1,8 @@
+import { ChangeEvent } from "react";
+
+export interface SearchBarProps {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  className?: string;
+}
