45c4fcf909194ac3f0672434a972288c6e7ff6a8
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-12 00:20:34 +0700

    add: Create EditObatPage component for editing medication details

---
+++ app/dashboard/obat/edit/[id]/page.tsx
@@ -0,0 +1,13 @@
+import EditObatForm from "./EditObatForm";
+
+export default function EditObatPage() {
+  return (
+    <div className="wrapper">
+      <h2 className="text-2xl font-bold mb-1">Ubah Obat</h2>
+      <p className="text-primary mb-6">
+        Lengkapi informasi berikut untuk mengubah obat
+      </p>
+      <EditObatForm />
+    </div>
+  );
+}
