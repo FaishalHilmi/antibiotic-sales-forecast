bdfc8bb967caad8dcb4cd041b0de57799893073f
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-13 19:48:03 +0700

    add: Create TambahObatPage component for adding medication

---
+++ app/dashboard/obat/tambah/page.tsx
@@ -0,0 +1,13 @@
+import TambahObatForm from "./TambahObatForm";
+
+export default function TambahObatPage() {
+  return (
+    <div className="wrapper">
+      <h2 className="text-2xl font-bold mb-1">Tambah Obat</h2>
+      <p className="text-primary mb-6">
+        Lengkapi informasi berikut untuk menambahkan obat
+      </p>
+      <TambahObatForm />
+    </div>
+  );
+}
