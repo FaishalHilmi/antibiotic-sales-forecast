1ea067de3eb160fd8880edb7cd621dc6ed0cd0a6
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-13 21:34:25 +0700

    fix: Update import and usage of DetailObat to DetailObatView for consistency

--- app/dashboard/obat/[id]/page.tsx
+++ app/dashboard/obat/[id]/page.tsx
@@ -1,11 +1,11 @@
-import DetailObat from "./DetailObat";
+import DetailObatView from "./DetailObatView";
 
 export default function DetailObatPage() {
   return (
     <div className="wrapper">
       <h2 className="text-2xl font-bold mb-1">Detail Obat</h2>
       <p className="text-primary mb-6">Lihat informasi lengkap mengenai obat</p>
-      <DetailObat />
+      <DetailObatView />
     </div>
   );
 }
