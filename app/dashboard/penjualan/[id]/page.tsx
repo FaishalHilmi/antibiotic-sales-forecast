a7c6c998a45f2ca790b1dd71b5fe801d95ca539d
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-14 18:54:51 +0700

    refactor: Simplify DetailTransaksiPage by removing unused state for detail transaksi

---
+++ app/dashboard/penjualan/[id]/page.tsx
@@ -0,0 +1,13 @@
+import DetailTransaksiView from "./DetailTransaksiView";
+
+export default function DetailTransaksiPage() {
+  return (
+    <div className="wrapper">
+      <h2 className="text-2xl font-bold mb-1">Detail Transaksi Penjualan</h2>
+      <p className="text-primary mb-6">
+        Lihat informasi lengkap mengenai transaksi
+      </p>
+      <DetailTransaksiView />
+    </div>
+  );
+}
