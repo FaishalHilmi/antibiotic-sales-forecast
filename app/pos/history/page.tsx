50444a2cfa9516e48505e8f97fdeca504bd89643
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-15 00:23:20 +0700

    refactor: Simplify HistoryPage to import component RiwayatPenjualanView

--- app/pos/history/page.tsx
+++ app/pos/history/page.tsx
@@ -1,33 +1,6 @@
-"use client";
-
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
-import { transactions } from "@/data/transaction";
-import { transactionColumn } from "@/column/pos/transactionColumn";
-
-// Import DataTable secara dinamis (SSR disabled)
-const DataTable = dynamic(() => import("react-data-table-component"), {
-  ssr: false,
-});
+import RiwayatPenjualanView from "./RiwayatPenjualanView";
 
 export default function History() {
-  const [search, setSearch] = useState("");
-
-  const customStyles = {
-    headCells: {
-      style: {
-        fontWeight: "700", // Bold
-        fontSize: "14px", // Ukuran font
-      },
-    },
-  };
-
-  const filteredData = transactions.filter(
-    (item) =>
-      item.id.toLowerCase().includes(search.toLowerCase()) ||
-      item.cashier.toLowerCase().includes(search.toLowerCase())
-  );
-
   return (
     <div>
       <div className="wrapper">
@@ -37,23 +10,7 @@ export default function History() {
         <span className="block text-primary">
           Catatan lengkap dari penjualan yang telah diselesaikan
         </span>
-        <div className="table-wrapper p-4 mt-7 bg-white flex flex-col items-end border border-gray-200 rounded-3xl">
-          <input
-            type="text"
-            placeholder="Cari ID atau Nama Kasir..."
-            className="border border-gray-200 rounded-xl px-3 py-2 mb-4 text-sm outline-gray-300"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <DataTable
-            columns={transactionColumn}
-            data={filteredData}
-            pagination
-            highlightOnHover
-            responsive
-            customStyles={customStyles}
-          />
-        </div>
+        <RiwayatPenjualanView />
       </div>
     </div>
   );
