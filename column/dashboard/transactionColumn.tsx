d270059ec900e34ab6fe4ded9e4d7fd169ee653b
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-15 23:21:36 +0700

    chore: Change method toLocalString to be Intl.NumberFormat

---
+++ column/dashboard/transactionColumn.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+export const transactionColumn = [
+  {
+    name: "ID Transaksi",
+    selector: (row: any) => row.id,
+  },
+  {
+    name: "Tanggal Transaksi",
+    selector: (row: any) => row.date,
+  },
+  {
+    name: "Total Harga",
+    selector: (row: any) =>
+      new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0,
+      }).format(row.total),
+  },
+  {
+    name: "Nama Kasir",
+    selector: (row: any) => row.cashier,
+  },
+  {
+    name: "Aksi",
+    cell: (row: any) => (
+      <div className="flex flex-col md:flex-row gap-1 md:gap-2 py-2 md:py-0">
+        <Link
+          href={`/dashboard/penjualan/${row.id}`}
+          className="bg-blue-600 text-white text-xs px-3 py-2 rounded-lg"
+        >
+          Detail
+        </Link>
+        <Link
+          href={`dashboard/penjualan/delete/${row.id}`}
+          className="bg-red-500 text-white text-xs px-3 py-2 rounded-lg"
+        >
+          Hapus
+        </Link>
+      </div>
+    ),
+  },
+];
