f6fa27f6def4055bddb293172c7e632dd0cc76ea
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-19 22:31:04 +0700

    add: InformasiUmum component to display general information

---
+++ app/dashboard/rekap-penjualan/[id]/components/InformasiUmum.tsx
@@ -0,0 +1,40 @@
+export default function InformasiUmum({
+  createdAt,
+  jumlahTransaksi,
+  totalObat,
+  totalHarga,
+  topObat,
+}: {
+  createdAt: string;
+  jumlahTransaksi: number;
+  totalObat: number;
+  totalHarga: number;
+  topObat: string;
+}) {
+  return (
+    <table className="text-sm w-full table-fixed">
+      <tbody className="space-y-1">
+        <tr>
+          <td className="font-semibold w-40">Tanggal Dibuat</td>
+          <td>: {createdAt}</td>
+        </tr>
+        <tr>
+          <td className="font-semibold">Jumlah Transaksi</td>
+          <td>: {jumlahTransaksi}</td>
+        </tr>
+        <tr>
+          <td className="font-semibold">Total Obat Terjual</td>
+          <td>: {totalObat} strip</td>
+        </tr>
+        <tr>
+          <td className="font-semibold">Pendapatan Kotor</td>
+          <td>: Rp {totalHarga.toLocaleString("id-ID")}</td>
+        </tr>
+        <tr>
+          <td className="font-semibold">Obat Terbanyak Terjual</td>
+          <td>: {topObat}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
