06e8fcbf21cae8cf6be13e3eafafec06e92b8832
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-19 22:38:31 +0700

    add: Custom Styles for Datatable

---
+++ components/datatable/tableStyle.tsx
@@ -0,0 +1,16 @@
+import { TableStyles } from "react-data-table-component";
+
+export const customDataTableStyles: TableStyles = {
+  headCells: {
+    style: {
+      fontWeight: "700",
+      fontSize: "14px",
+      backgroundColor: "#f3f4f6",
+    },
+  },
+  rows: {
+    style: {
+      fontSize: "14px",
+    },
+  },
+};
