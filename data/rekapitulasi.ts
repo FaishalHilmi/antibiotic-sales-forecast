87e30df3296b4199cdd07cc13c23feff4ceaef97
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-19 22:37:52 +0700

    add: data dummy of pengeluaranData and peramalanData for detail recap page

---
+++ data/rekapitulasi.ts
@@ -0,0 +1,53 @@
+// Dummy data obat terlaris
+export const topObatData = [
+  { nama: "Paracetamol", total: 210 },
+  { nama: "Amoxicillin", total: 180 },
+  { nama: "Vitamin C", total: 150 },
+  { nama: "Ibuprofen", total: 130 },
+  { nama: "Cetirizine", total: 120 },
+];
+
+// Data
+export const dataObat = [
+  { nama: "Paracetamol", jumlah: 10, total: 50000 },
+  { nama: "Amoxicillin", jumlah: 5, total: 40000 },
+  { nama: "Vitamin C", jumlah: 7, total: 21000 },
+];
+
+export const pengeluaranData = [
+  {
+    nama: "Paracetamol",
+    minggu_4: 25,
+    minggu_3: 18,
+    minggu_2: 22,
+    minggu_1: 28,
+    total: 93,
+  },
+  {
+    nama: "Amoxicillin",
+    minggu_4: 15,
+    minggu_3: 20,
+    minggu_2: 19,
+    minggu_1: 16,
+    total: 70,
+  },
+];
+
+export const peramalanData = [
+  {
+    nama: "Paracetamol",
+    minggu_1: 27,
+    minggu_2: 26,
+    minggu_3: 30,
+    minggu_4: 29,
+    total: 112,
+  },
+  {
+    nama: "Amoxicillin",
+    minggu_1: 17,
+    minggu_2: 18,
+    minggu_3: 19,
+    minggu_4: 20,
+    total: 74,
+  },
+];
