1e4856f2ebc681534445f06071e126a056cf9e6a
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-19 22:29:37 +0700

    add: HeaderRekap Component to Heading Recap Letter

---
+++ app/dashboard/rekap-penjualan/[id]/components/HeaderRekap.tsx
@@ -0,0 +1,12 @@
+export default function HeaderRekap() {
+  return (
+    <div className="text-center border-b-2 border-black pb-4 mb-4">
+      <h1 className="text-2xl font-bold uppercase">APOTEK FAJAR CEMPAKA</h1>
+      <p className="text-sm">
+        Jl. Cempaka No.35, Sari Rejo, Kec. Medan Polonia, Kota Medan, Sumatera
+        Utara
+      </p>
+      <p className="text-sm">Telp: (061) 1234567</p>
+    </div>
+  );
+}
