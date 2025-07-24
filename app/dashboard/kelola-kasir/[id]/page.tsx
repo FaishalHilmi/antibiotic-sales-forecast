9ee06913e6209b54c1a3db619e29354278fd2be5
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-24 19:09:04 +0700

    add: DetailKasirPage Component to Import DetailKasirView Component

---
+++ app/dashboard/kelola-kasir/[id]/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+import DetailKasirView from "./DetailKasirView";
+
+export default function DetailKasirPage() {
+  return (
+    <div className="wrapper">
+      <h2 className="text-2xl font-bold mb-1">Detail Akun Kasir</h2>
+      <p className="text-primary mb-6">
+        Lihat informasi lengkap mengenai akun kasir
+      </p>
+      <div className="mb-6">
+        <Link
+          href={"/dashboard/kelola-kasir"}
+          className="px-4 py-2 bg-gray-500 text-white rounded-md"
+        >
+          Kembali
+        </Link>
+      </div>
+      <DetailKasirView />
+    </div>
+  );
+}
