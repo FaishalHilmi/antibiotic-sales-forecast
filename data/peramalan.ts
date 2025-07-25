9a00684368a5c3b15c1b7c803abdc149b77fedf3
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-25 15:15:28 +0700

    add: Dummy Data Riwayat Peramalan

---
+++ data/peramalan.ts
@@ -0,0 +1,48 @@
+export const peramalanDummyData = [
+  {
+    id: 1,
+    name: "Amoxicillin",
+    category: "Antibiotik",
+  },
+  {
+    id: 2,
+    name: "Ramoxicillin",
+    category: "Antibiotik",
+  },
+  {
+    id: 3,
+    name: "Pamoxicillin",
+    category: "Antibiotik",
+  },
+];
+
+export const riwayatPeramalanDummyData = [
+  { periode: "Minggu 1", aktual: 120, peramalan: null, mape: null, mae: null },
+  { periode: "Minggu 2", aktual: 130, peramalan: null, mape: null, mae: null },
+  { periode: "Minggu 3", aktual: 125, peramalan: null, mape: null, mae: null },
+  { periode: "Minggu 4", aktual: 135, peramalan: null, mape: null, mae: null },
+  { periode: "Minggu 5", aktual: 140, peramalan: 127.5, mape: 8.93, mae: 12.5 },
+  { periode: "Minggu 6", aktual: 145, peramalan: 132.5, mape: 8.62, mae: 12.5 },
+  {
+    periode: "Minggu 7",
+    aktual: 150,
+    peramalan: 138.75,
+    mape: 7.5,
+    mae: 11.25,
+  },
+  { periode: "Minggu 8", aktual: 148, peramalan: 142.5, mape: 3.72, mae: 5.5 },
+  {
+    periode: "Minggu 9",
+    aktual: 152,
+    peramalan: 146.25,
+    mape: 3.78,
+    mae: 5.75,
+  },
+  {
+    periode: "Minggu 10",
+    aktual: 155,
+    peramalan: 148.75,
+    mape: 4.03,
+    mae: 6.25,
+  },
+];
