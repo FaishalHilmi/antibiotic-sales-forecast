7d9d0056871fcd838df3fcfaaf4c516e250d8f3e
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-25 15:14:32 +0700

    add: Dummy Data Cashier

---
+++ data/kasir.ts
@@ -0,0 +1,20 @@
+export const dummyDataCashier = [
+  {
+    id: 1,
+    gambar: "https://via.placeholder.com/150",
+    name: "Kasir 1",
+    username: "kasir1",
+    password: "password1",
+    role: "Kasir",
+    created_at: "2023-10-01",
+  },
+  {
+    id: 2,
+    gambar: "https://via.placeholder.com/150",
+    name: "Kasir 2",
+    username: "kasir2",
+    password: "password2",
+    role: "Kasir",
+    created_at: "2023-10-02",
+  },
+];
