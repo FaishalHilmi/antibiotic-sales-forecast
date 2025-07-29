43b678abaf0416c565d7c8b95e624497e9034e1b
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-29 23:41:06 +0700

    chore: Import SessionWrapper in Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const geistSans = Geist({
   weight: ["400", "500", "600", "700"],
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.className} antialiased`}>{children}</body>
+      <body className={`${geistSans.className} antialiased`}>
+        <SessionWrapper>{children}</SessionWrapper>
+      </body>
     </html>
   );
 }
