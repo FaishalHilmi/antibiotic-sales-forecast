291b77fba466c9c2a3a634523ac796ddea94aa19
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-07-29 23:40:21 +0700

    add: NextAuth for Login

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,74 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { Credentials } from "@/types/next-auth";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials: Credentials | undefined) {
+        if (!credentials) {
+          throw new Error("Kredensial tidak ditemukan");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { username: credentials.username },
+        });
+
+        if (!user) throw new Error("Username tidak ditemukan");
+
+        const isPasswordValid = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isPasswordValid) throw new Error("Password salah");
+
+        return {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 60,
+  },
+  callbacks: {
+    async jwt({ token, user }: any) {
+      if (user) {
+        token.role = user.role;
+        token.username = user.username;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }: any) {
+      if (token) {
+        session.user.role = token.role;
+        session.user.username = token.username;
+      }
+
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
