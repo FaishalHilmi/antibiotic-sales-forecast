7eb7567a783c8c1daa9128c3d715729067395210
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-03 22:26:28 +0700

    add: next-auth type definition

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,30 @@
+import NextAuth, { DefaultSession } from "next-auth";
+
 export interface Credentials {
   username: string;
   password: string;
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      username: string;
+      role: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    username: string;
+    role: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    username: string;
+    role: string;
+  }
 }
