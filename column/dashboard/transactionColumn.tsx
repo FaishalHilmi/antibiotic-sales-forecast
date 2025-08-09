be3ecd6991a964e58ad568c653cd8e19f0c19de7
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-09 23:27:16 +0700

    chore: change using format date and currency, and add handleDelete

--- column/dashboard/transactionColumn.tsx
+++ column/dashboard/transactionColumn.tsx
@@ -1,28 +1,25 @@
 "use client";
 
+import { formatTanggal } from "@/utils/date";
+import { formatRupiah } from "@/utils/formatCurrency";
 import Link from "next/link";
 
-export const transactionColumn = [
+export const transactionColumn = (handleDelete: (id: string) => void) => [
   {
     name: "ID Transaksi",
     selector: (row: any) => row.id,
   },
   {
     name: "Tanggal Transaksi",
-    selector: (row: any) => row.date,
+    selector: (row: any) => formatTanggal(row.createdAt),
   },
   {
     name: "Total Harga",
-    selector: (row: any) =>
-      new Intl.NumberFormat("id-ID", {
-        style: "currency",
-        currency: "IDR",
-        minimumFractionDigits: 0,
-      }).format(row.total),
+    selector: (row: any) => formatRupiah(Number(row.totalAmount)),
   },
   {
     name: "Nama Kasir",
-    selector: (row: any) => row.cashier,
+    selector: (row: any) => row.cashier.name,
   },
   {
     name: "Aksi",
@@ -31,15 +28,16 @@ export const transactionColumn = [
         <Link
           href={`/dashboard/transaksi/${row.id}`}
           className="bg-blue-600 text-white text-xs px-3 py-2 rounded-lg"
+          style={{ background: "#155DFC" }}
         >
           Detail
         </Link>
-        <Link
-          href={`dashboard/transaksi/delete/${row.id}`}
+        <button
+          onClick={() => handleDelete(row.id)}
           className="bg-red-500 text-white text-xs px-3 py-2 rounded-lg"
         >
           Hapus
-        </Link>
+        </button>
       </div>
     ),
   },
