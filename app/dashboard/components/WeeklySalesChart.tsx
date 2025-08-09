e5fee690ac679b88cbd60a39e9fa000c44d7538e
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-09 22:13:27 +0700

    feat: use weekly sales data from fetching API

--- app/dashboard/components/WeeklySalesChart.tsx
+++ app/dashboard/components/WeeklySalesChart.tsx
@@ -1,13 +1,14 @@
 import BarChart from "@/components/BarChart";
+import { weeklySalesProps } from "@/types/sales";
 
-export default function WeeklySalesChart() {
+export default function WeeklySalesChart({ weeklySales }: weeklySalesProps) {
   return (
     <div className="col-span-1 md:col-span-2">
       <div className="cart-wrapper bg-white border border-gray-200 p-3 md:p-4 rounded-2xl">
         <h1 className="font-semibold text-lg md:text-xl -mb-4">
           Penjualan Mingguan
         </h1>
-        <BarChart />
+        <BarChart weeklySales={weeklySales} />
       </div>
     </div>
   );
