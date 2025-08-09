72334755c14059912864e8ff65e09d34f9836f8d
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-09 21:19:38 +0700

    add: Cashier, LatestTransaction, DataSummarya and summaryProps type identify

--- types/summary.d.ts
+++ types/summary.d.ts
@@ -1,5 +1,32 @@
-export interface lastSevenDaysProps {
+export interface LastSevenDaysProps {
   date: string;
   day: string;
   total: number;
+}
+
+export interface Cashier {
+  name: string;
+}
+
+export interface LatestTransactions {
+  id: string;
+  cashierId: number;
+  totalItems: number;
+  totalAmount: string;
+  paymentMethod: string;
+  createdAt: string;
+  deletedAt: string | null;
+  cashier: Cashier;
+}
+
+export interface DataSummary {
+  total_item: number | null;
+  total_revenue: number | null;
+  total_medicines: number | null;
+  seven_days_sales: lastSevenDaysProps[];
+  latest_transactions: LatestTransactions[];
+}
+
+export interface summaryProps {
+  summary: DataSummary;
 }
