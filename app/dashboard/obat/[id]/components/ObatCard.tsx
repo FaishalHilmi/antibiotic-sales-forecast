9013577e4aec17a784c6e2d51b1a3543f298b9d5
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-09 22:09:37 +0700

    feat: use data medicines from fetching API

--- app/dashboard/obat/[id]/components/ObatCard.tsx
+++ app/dashboard/obat/[id]/components/ObatCard.tsx
@@ -1,16 +1,23 @@
-import { MedicineWithImage } from "@/types/medicine";
+import { MedicineProps } from "@/types/medicine";
+import Image from "next/image";
 import React from "react";
 
-export default function ObatCard({ obat }: { obat: MedicineWithImage }) {
+export default function ObatCard({ medicines }: MedicineProps) {
   return (
     <div className="bg-white p-6 rounded-3xl border border-gray-200">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 items-start">
         {/* Gambar */}
         <div className="md:col-span-1">
-          <img
-            src={obat.image}
-            alt={obat.name}
-            className="w-full h-auto max-h-56 object-cover rounded-2xl border"
+          <Image
+            src={
+              medicines.imagePath == ""
+                ? "/blank-image.png"
+                : medicines.imagePath
+            }
+            alt={medicines.name}
+            className="w-full h-40 object-cover rounded-2xl border"
+            width={500}
+            height={200}
           />
         </div>
 
@@ -18,17 +25,17 @@ export default function ObatCard({ obat }: { obat: MedicineWithImage }) {
         <div className="md:col-span-2 flex flex-col justify-between gap-4">
           <div>
             <h3 className="text-xl font-semibold text-gray-800 mb-2">
-              {obat.name}
+              {medicines.name}
             </h3>
             <div className="flex flex-col gap-2 text-sm text-gray-700">
               <div className="flex items-center gap-2">
                 <span className="font-medium w-24">Kategori</span>:
-                <span>{obat.category}</span>
+                <span className="capitalize">{medicines.category}</span>
               </div>
               <div className="flex items-center gap-2">
                 <span className="font-medium w-24">Harga</span>:
                 <span className="text-green-600 font-semibold">
-                  Rp {obat.price.toLocaleString()}
+                  Rp {medicines.price.toLocaleString()}
                 </span>
               </div>
               <div className="flex items-center gap-2">
@@ -38,7 +45,7 @@ export default function ObatCard({ obat }: { obat: MedicineWithImage }) {
               <div className="flex items-center gap-2">
                 <span className="font-medium w-24">Stok</span>:
                 <span className="text-blue-600 font-semibold">
-                  {obat.stocks}
+                  {medicines.stock}
                 </span>
               </div>
             </div>
