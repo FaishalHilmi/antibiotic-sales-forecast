fe3472e1c23c4187df5cea3f78f5d14a9f9f502b
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-09 23:24:43 +0700

    feat: fetching data summary from API

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,36 @@
+import { cookies } from "next/headers";
 import DashboardView from "./DashboardView";
+import { DataSummary } from "@/types/summary";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const cookieStore = await cookies();
+  const session = await getServerSession(authOptions);
+  const name = session?.user?.name;
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/summary`, {
+    method: "GET",
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw Error("Gagal fetch API");
+  }
+
+  const req = await res.json();
+  const dataSummary: DataSummary = req.payload;
+
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-1">Halo, Admin 👋</h2>
+      <h2 className="text-2xl font-bold mb-1 capitalize">Halo, {name} 👋</h2>
       <p className="text-gray-500 mb-4 md:mb-6">
         Ini ringkasan penjualan dan aktivitas hari ini.
       </p>
-      <DashboardView />
+      <DashboardView summary={dataSummary} />
     </div>
   );
 }
