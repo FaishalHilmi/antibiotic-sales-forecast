1f6a3880ce93ff3c82556fa151b25d5da230ce3a
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-10 17:12:44 +0700

    feat: fetching API fro data transactions

--- app/dashboard/transaksi/page.tsx
+++ app/dashboard/transaksi/page.tsx
@@ -1,6 +1,26 @@
+import { cookies } from "next/headers";
 import TransaksiView from "./TransaksiView";
 
-export default function KelolaTransaksiPage() {
+export default async function KelolaTransaksiPage() {
+  const cookieStore = await cookies();
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/transaction`,
+    {
+      method: "GET",
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw Error("Gagal fetch API");
+  }
+
+  const req = await res.json();
+  const transactions = req.payload;
   return (
     <div>
       <div className="wrapper">
@@ -9,7 +29,7 @@ export default function KelolaTransaksiPage() {
           Lihat dan kelola data transaksi penjualan yang tersedia
         </p>
         <div className="table-wrapper p-4 mt-7 bg-white flex flex-col border border-gray-200 rounded-3xl">
-          <TransaksiView />
+          <TransaksiView transactions={transactions} />
         </div>
       </div>
     </div>
