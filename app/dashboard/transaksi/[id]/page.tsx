44c34adf2073f79e9922951a2cd2241cf0af5034
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-10 17:10:37 +0700

    feat: fetching API for detail transactions

--- app/dashboard/transaksi/[id]/page.tsx
+++ app/dashboard/transaksi/[id]/page.tsx
@@ -1,13 +1,37 @@
+import { cookies } from "next/headers";
 import DetailTransaksiView from "./DetailTransaksiView";
+import { DataTransaction } from "@/types/transaction";
 
-export default function DetailTransaksiPage() {
+export default async function DetailTransaksiPage(props: {
+  params: Promise<{ id: string }>;
+}) {
+  const cookieStore = await cookies();
+  const { id } = await props.params;
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/transaction/${id}`,
+    {
+      method: "GET",
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+      cache: "no-store",
+    }
+  );
+
+  if (!res) {
+    throw new Error("Gagal fetch API");
+  }
+
+  const req = await res.json();
+  const dataTransaction: DataTransaction = await req.payload;
   return (
     <div className="wrapper">
       <h2 className="text-2xl font-bold mb-1">Detail Transaksi Penjualan</h2>
       <p className="text-primary mb-6">
         Lihat informasi lengkap mengenai transaksi
       </p>
-      <DetailTransaksiView />
+      <DetailTransaksiView transaction={dataTransaction} />
     </div>
   );
 }
