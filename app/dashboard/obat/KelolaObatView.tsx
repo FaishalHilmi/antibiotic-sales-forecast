e523a6186c3de8119641679187e3cd9c2804128f
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-10 17:19:12 +0700

    feat: consume data medicines from API and add modal confirm delete

--- app/dashboard/obat/KelolaObatView.tsx
+++ app/dashboard/obat/KelolaObatView.tsx
@@ -3,20 +3,41 @@
 import SearchBar from "@/components/SearchBar";
 import dynamic from "next/dynamic";
 import { useState } from "react";
-import { dummyObat } from "@/data/obat";
 import { medicineColumn } from "@/column/dashboard/medicineColumn";
 import { headersBoldStyle } from "@/components/datatable/headersBoldStyle";
 import Link from "next/link";
+import { MedicineState } from "@/types/medicine";
+import Modal from "@/components/Modal";
+import { useDeleteWithConfirm } from "@/hooks/useDeletedConfirm";
 
 // Import DataTable secara dinamis (SSR disabled)
 const DataTable = dynamic(() => import("react-data-table-component"), {
   ssr: false,
 });
 
-export default function KelolaObatView() {
+export default function KelolaObatView({
+  medicines,
+}: {
+  medicines: MedicineState[];
+}) {
+  const [dataMedicines, setDataMedicines] =
+    useState<MedicineState[]>(medicines);
   const [search, setSearch] = useState<string>("");
 
-  const filteredData = dummyObat.filter((item) =>
+  const {
+    isModalOpen,
+    isDeleting,
+    handleDelete,
+    handleConfirmDelete,
+    setIsModalOpen,
+  } = useDeleteWithConfirm({
+    endpoint: "/api/medicines",
+    onSuccess: (id) => {
+      setDataMedicines((prev) => prev.filter((item) => item.id !== id));
+    },
+  });
+
+  const filteredDataMedicine = dataMedicines.filter((item) =>
     item.name.toLowerCase().includes(search.toLowerCase())
   );
 
@@ -37,13 +58,40 @@ export default function KelolaObatView() {
         />
       </div>
       <DataTable
-        columns={medicineColumn}
-        data={filteredData}
+        columns={medicineColumn(handleDelete)}
+        data={filteredDataMedicine}
         pagination
         highlightOnHover
         responsive
         customStyles={headersBoldStyle}
       />
+
+      {/* MODAL KONFIRMASI */}
+      <Modal
+        isOpen={isModalOpen}
+        onCloseAction={() => !isDeleting && setIsModalOpen(false)}
+        title="Konfirmasi Penghapusan"
+      >
+        <p className="text-center mb-4">
+          Apakah Anda yakin ingin menghapus obat ini?
+        </p>
+        <div className="flex justify-center gap-4">
+          <button
+            className="bg-gray-300 text-black px-4 py-2 rounded-lg hover:bg-gray-400 disabled:opacity-50"
+            onClick={() => setIsModalOpen(false)}
+            disabled={isDeleting}
+          >
+            Batal
+          </button>
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 disabled:opacity-50"
+            onClick={handleConfirmDelete}
+            disabled={isDeleting}
+          >
+            {isDeleting ? "Menghapus..." : "Ya, Hapus"}
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 }
