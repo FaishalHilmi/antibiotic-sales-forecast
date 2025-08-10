e1a4947e1c4142defc803547baca6e7b6e18f252
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-10 17:17:45 +0700

    feat: fetching API for get all medicines data

--- app/dashboard/obat/page.tsx
+++ app/dashboard/obat/page.tsx
@@ -1,6 +1,25 @@
+import { MedicineState } from "@/types/medicine";
 import KelolaObatView from "./KelolaObatView";
+import { cookies } from "next/headers";
 
-export default function KelolaObatpage() {
+export default async function KelolaObatpage() {
+  const cookieStore = await cookies();
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/medicines`, {
+    method: "GET",
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error(`Gagal Fetch API - Status ${res.status}`);
+  }
+
+  const req = await res.json();
+  const medicines = req.payload;
+
   return (
     <div>
       <div className="wrapper">
@@ -8,7 +27,7 @@ export default function KelolaObatpage() {
         <p className="text-primary mb-4">
           Lihat dan kelola data obat yang tersedia
         </p>
-        <KelolaObatView />
+        <KelolaObatView medicines={medicines} />
       </div>
     </div>
   );
