857c596ed1c20b704022e8700a4aa6b6648d41ec
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-10 17:11:27 +0700

    feat: consume data transaction from API

--- app/dashboard/transaksi/[id]/DetailTransaksiView.tsx
+++ app/dashboard/transaksi/[id]/DetailTransaksiView.tsx
@@ -5,43 +5,19 @@ import Link from "next/link";
 import { MoveLeft, Printer } from "lucide-react";
 import TransactionReceipt from "@/components/TransactionReceipt";
 import { headersBoldStyle } from "@/components/datatable/headersBoldStyle";
+import { TransactionProps } from "@/types/transaction";
+import { formatTanggal, formatWaktuWIB } from "@/utils/date";
+import { itemSummaryColumn } from "@/column/pos/transactionColumn";
+import { formatRupiah } from "@/utils/formatCurrency";
 
 const DataTable = dynamic(() => import("react-data-table-component"), {
   ssr: false,
 });
 
-export default function DetailTransaksiView() {
-  // Dummy Data
-  const data = [
-    { itemName: "Paracetamol", quantity: 2, price: 5000, total: 10000 },
-    { itemName: "Amoxicillin", quantity: 1, price: 8000, total: 8000 },
-    { itemName: "Vitamin C", quantity: 3, price: 4000, total: 12000 },
-  ];
-
-  const subTotal = data.reduce((sum, item) => sum + item.total, 0);
-
-  const receiptData = {
-    transactionId: "AB13132",
-    datetime: "Kamis, 17 Desember 2025 14:24 WIB",
-    cashierName: "Faishal Hilmy",
-    items: data,
-    subTotal,
-  };
-
-  const columns = [
-    { name: "Nama Item", selector: (row: any) => row.itemName, sortable: true },
-    { name: "Jumlah", selector: (row: any) => row.quantity, sortable: true },
-    {
-      name: "Harga",
-      selector: (row: any) => `Rp ${row.price.toLocaleString()}`,
-      sortable: true,
-    },
-    {
-      name: "Total Harga",
-      selector: (row: any) => `Rp ${row.total.toLocaleString()}`,
-      sortable: true,
-    },
-  ];
+export default function DetailTransaksiView({ transaction }: TransactionProps) {
+  const date = formatTanggal(transaction.createdAt);
+  const hour = formatWaktuWIB(transaction.createdAt);
+  const datetime = `${date} ${hour}`;
 
   const handlePrint = () => window.print();
 
@@ -61,7 +37,8 @@ export default function DetailTransaksiView() {
           <div className="mobile-section flex flex-row justify-between items-center gap-2 md:hidden">
             <div className="id-transaction md:hidden">
               <span className="font-bold text-sm">
-                ID Transaksi <span className="text-primary">AB13132</span>
+                ID Transaksi{" "}
+                <span className="text-primary">{transaction.id}</span>
               </span>
             </div>
             <div className="button-action flex gap-2">
@@ -76,7 +53,8 @@ export default function DetailTransaksiView() {
           </div>
           <div className="id-transaction hidden md:inline">
             <span className="font-bold">
-              ID Transaksi <span className="text-primary">AB13132</span>
+              ID Transaksi{" "}
+              <span className="text-primary">{transaction.id}</span>
             </span>
           </div>
           <div className="button-action gap-2 hidden md:flex">
@@ -95,8 +73,8 @@ export default function DetailTransaksiView() {
           <div className="col-span-1 md:col-span-2 border border-gray-200 rounded-xl p-3 md:p-4">
             <h1 className="font-bold text-lg md:text-xl mb-3">Rincian Item</h1>
             <DataTable
-              columns={columns}
-              data={data}
+              columns={itemSummaryColumn}
+              data={transaction.details}
               pagination
               highlightOnHover
               responsive
@@ -109,24 +87,24 @@ export default function DetailTransaksiView() {
             <div className="flex flex-col gap-2 text-sm">
               <div className="flex justify-between">
                 <span className="font-bold">Tanggal Pemesanan</span>
-                <span className="text-end">Kamis, 17 Desember 2025</span>
+                <span className="text-end">{date}</span>
               </div>
               <div className="flex justify-between">
                 <span className="font-bold">Waktu Transaksi</span>
-                <span>14:24 WIB</span>
+                <span>{hour}</span>
               </div>
               <div className="flex justify-between">
                 <span className="font-bold">Jumlah Item</span>
-                <span>6 Item</span>
+                <span>{transaction.totalItems} Item</span>
               </div>
               <div className="flex justify-between">
                 <span className="font-bold">Metode Pembayaran</span>
-                <span>Tunai</span>
+                <span className="uppercase">{transaction.paymentMethod}</span>
               </div>
             </div>
             <div className="w-full flex justify-between bg-primary-dark text-white font-bold text-base p-3 rounded-lg">
               <span>Total</span>
-              <span>Rp 80.000</span>
+              <span>{formatRupiah(Number(transaction.totalAmount))}</span>
             </div>
           </div>
         </div>
@@ -134,7 +112,18 @@ export default function DetailTransaksiView() {
 
       {/* Komponen Struk untuk Cetak */}
       <div className="receipt-print hidden print:block" id="print-area">
-        <TransactionReceipt {...receiptData} />
+        <TransactionReceipt
+          transactionId={transaction.id}
+          datetime={datetime}
+          cashierName={transaction.cashier.name}
+          items={transaction.details.map((item) => ({
+            itemName: item.medicine.name,
+            quantity: item.quantity,
+            price: parseInt(item.unitPrice),
+            total: parseInt(item.subtotal),
+          }))}
+          subTotal={parseInt(transaction.totalAmount)}
+        />
       </div>
     </div>
   );
