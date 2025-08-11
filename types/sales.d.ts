fe93f5cbd25b9d648e17171665e56513d4965057
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-11 23:21:28 +0700

    add: sales recap, recap detail, sale recap and data top five medicine type identify

--- types/sales.d.ts
+++ types/sales.d.ts
@@ -3,3 +3,37 @@ import { LastSevenDaysProps } from "./summary";
 export interface weeklySalesProps {
   weeklySales: LastSevenDaysProps[];
 }
+
+export interface SalesRecap {
+  id: number;
+  month: number;
+  year: number;
+  totalTransactions: number;
+  totalSoldQuantity: number;
+  grossRevenue: string;
+  topSellingMedicine: string;
+  createdAt: string;
+  updatedAt: string;
+  medicineId: number;
+}
+
+export interface RecapDetail {
+  id: number;
+  salesRecapId: number;
+  quantitySold: number;
+  unitPrice: string;
+  totalRevenue: string;
+  medicine: {
+    name: string;
+    unit: string;
+  };
+}
+
+export interface SaleRecap extends SalesRecap {
+  recapDetails: RecapDetail[];
+}
+
+export interface DataTopFiveMedicines {
+  name: string;
+  quantity: number;
+}
