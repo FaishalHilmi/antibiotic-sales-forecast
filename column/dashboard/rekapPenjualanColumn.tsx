7e199753ba7659bd1a5e99128eeaa39c9e1230a7
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-11 23:12:55 +0700

    chore: change using format rupiah and add handle update

--- column/dashboard/rekapPenjualanColumn.tsx
+++ column/dashboard/rekapPenjualanColumn.tsx
@@ -1,28 +1,27 @@
 "use client";
 
+import { formatRupiah } from "@/utils/formatCurrency";
 import Link from "next/link";
 
-export const rekapPenjualanColumn = [
+export const salesRecapColumn = (
+  handleUpdateSalesRecap: (id: string) => void
+) => [
   {
     name: "No",
-    selector: (row: any) => row.id,
+    width: "60px",
+    selector: (_row: any, index: any) => index + 1,
   },
   {
     name: "Periode",
-    selector: (row: any) => row.periode,
+    selector: (row: any) => row.period,
   },
   {
     name: "Total Penjualan",
-    selector: (row: any) =>
-      new Intl.NumberFormat("id-ID", {
-        style: "currency",
-        currency: "IDR",
-        minimumFractionDigits: 0,
-      }).format(row.totalPenjualan),
+    selector: (row: any) => formatRupiah(row.grossRevenue),
   },
   {
     name: "Total Obat Terjual",
-    selector: (row: any) => row.totalObatTerjual,
+    selector: (row: any) => row.totalSoldQuantity,
   },
   {
     name: "Aksi",
@@ -31,27 +30,20 @@ export const rekapPenjualanColumn = [
         <Link
           href={`/dashboard/rekap-penjualan/${row.id}`}
           className="bg-blue-600 text-white text-xs text-center px-3 py-2 rounded-lg"
+          style={{ background: "#155DFC" }}
         >
           Detail
         </Link>
-        <Link
-          href={`dashboard/rekap-penjualan/update/${row.id}`}
+        <button
+          onClick={() => handleUpdateSalesRecap(row.id)}
           className="bg-yellow-500 text-white text-xs text-center px-3 py-2 rounded-lg"
+          style={{ background: "#F0B100" }}
         >
           Update
-        </Link>
+        </button>
       </div>
     ),
   },
-];
-
-export const rekapPengeluanColumn = [
-  { name: "Nama Obat", selector: (row: any) => row.nama },
-  { name: "Minggu 4", selector: (row: any) => row.minggu_4 },
-  { name: "Minggu 3", selector: (row: any) => row.minggu_3 },
-  { name: "Minggu 2", selector: (row: any) => row.minggu_2 },
-  { name: "Minggu 1", selector: (row: any) => row.minggu_1 },
-  { name: "Total", selector: (row: any) => row.total },
 ];
 
 export const rekapPenjualanObatColumn = [
@@ -62,32 +54,18 @@ export const rekapPenjualanObatColumn = [
   },
   {
     name: "Nama Obat",
-    selector: (row: any) => row.nama,
-    sortable: true,
+    selector: (row: any) => row.medicine.name,
   },
   {
     name: "Jumlah Terjual",
-    selector: (row: any) => `${row.jumlah} strip`,
-    sortable: true,
+    selector: (row: any) => `${row.quantitySold} ${row.medicine.unit}`,
   },
   {
     name: "Harga Satuan",
-    selector: (row: any) =>
-      `Rp ${(row.total / row.jumlah).toLocaleString("id-ID")}`,
-    sortable: false,
+    selector: (row: any) => formatRupiah(Number(row.unitPrice)),
   },
   {
     name: "Total Penjualan",
-    selector: (row: any) => `Rp ${row.total.toLocaleString("id-ID")}`,
-    sortable: true,
+    selector: (row: any) => formatRupiah(Number(row.totalRevenue)),
   },
-];
-
-export const rekapPeramalanColumn = [
-  { name: "Nama Obat", selector: (row: any) => row.nama },
-  { name: "Minggu 1", selector: (row: any) => row.minggu_1 },
-  { name: "Minggu 2", selector: (row: any) => row.minggu_2 },
-  { name: "Minggu 3", selector: (row: any) => row.minggu_3 },
-  { name: "Minggu 4", selector: (row: any) => row.minggu_4 },
-  { name: "Total", selector: (row: any) => row.total },
 ];
