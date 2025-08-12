27756561b85e73f3f04c3e4d607dcf96a66db827
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-12 21:10:51 +0700

    chore: using data of forecast result from api

--- chart/lineChartPeramalan.ts
+++ chart/lineChartPeramalan.ts
@@ -1,28 +1,30 @@
+import { ForecastResult } from "@/types/forecasting";
+
 export interface PeramalanItem {
   periode: string;
   aktual: number | null;
   peramalan: number | null;
 }
 
-export const getForecastChartOptions = (dataPeramalan: PeramalanItem[]) => {
+export const getForecastChartOptions = (forecastResult: ForecastResult[]) => {
   return {
     tooltip: { trigger: "axis" },
     legend: { data: ["Aktual", "Peramalan"] },
     xAxis: {
       type: "category",
-      data: dataPeramalan.map((d) => d.periode),
+      data: forecastResult.map((d) => d.periodLabel),
     },
     yAxis: { type: "value" },
     series: [
       {
         name: "Aktual",
         type: "line",
-        data: dataPeramalan.map((d) => d.aktual),
+        data: forecastResult.map((d) => d.actualValue),
       },
       {
         name: "Peramalan",
         type: "line",
-        data: dataPeramalan.map((d) => d.peramalan),
+        data: forecastResult.map((d) => d.forecastValue),
       },
     ],
   };
