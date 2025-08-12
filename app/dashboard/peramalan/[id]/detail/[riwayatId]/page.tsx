efbe72f80283980f1c51eeed87584a0c6806bebe
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-12 21:11:39 +0700

    feat: fetch API for forecasting result detail data

--- app/dashboard/peramalan/[id]/detail/[riwayatId]/page.tsx
+++ app/dashboard/peramalan/[id]/detail/[riwayatId]/page.tsx
@@ -1,7 +1,34 @@
 import Link from "next/link";
 import DetailRiwayatPeramalanView from "./DetailRiwayatPeramalanView";
+import { cookies } from "next/headers";
+import { error } from "console";
 
-export default function DetailRiwayatPeramalanPage() {
+export default async function DetailRiwayatPeramalanPage({
+  params,
+}: {
+  params: Promise<{ id: string; riwayatId: string }>;
+}) {
+  const cookieStore = await cookies();
+  const { id: medicineId, riwayatId: historyId } = await params;
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/forecast/${medicineId}/history/${historyId}`,
+    {
+      method: "GET",
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+      cache: "no-store",
+    }
+  );
+  const req = await res.json();
+
+  if (!res.ok) {
+    throw new Error(req.message || "Gagal fetch API");
+  }
+
+  const forecastingResultDetail = req.payload;
+
   return (
     <div>
       <div className="wrapper">
@@ -11,13 +38,15 @@ export default function DetailRiwayatPeramalanPage() {
         </p>
         <div className="mb-6">
           <Link
-            href={"/dashboard/peramalan"}
+            href={`/dashboard/peramalan/${medicineId}`}
             className="px-4 py-2 bg-gray-500 text-white rounded-md"
           >
             Kembali
           </Link>
         </div>
-        <DetailRiwayatPeramalanView />
+        <DetailRiwayatPeramalanView
+          forecastingResultDetail={forecastingResultDetail}
+        />
       </div>
     </div>
   );
