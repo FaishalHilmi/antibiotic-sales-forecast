4cfca469f7daa8b2b4610ac6e59d704e65d38a5b
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-14 23:47:55 +0700

    add: modal redirect to detail recap sales

--- app/dashboard/rekap-penjualan/RekapPenjualanView.tsx
+++ app/dashboard/rekap-penjualan/RekapPenjualanView.tsx
@@ -3,14 +3,15 @@
 import { salesRecapColumn } from "@/column/dashboard/rekapPenjualanColumn";
 import { useState } from "react";
 import { headersBoldStyle } from "@/components/datatable/headersBoldStyle";
-import { dummyRekapPenjualan } from "@/data/penjualan";
 import SearchBar from "@/components/SearchBar";
 import ModalTambahRekapitulasi from "./components/ModalTambahRekapitulasi";
 import dynamic from "next/dynamic";
 import { SalesRecap } from "@/types/sales";
 import { format } from "date-fns";
 import { id } from "date-fns/locale";
+import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import Modal from "@/components/Modal";
 
 const DataTable = dynamic(() => import("react-data-table-component"), {
   ssr: false,
@@ -23,7 +24,10 @@ export default function RekapPenjualanView({
 }) {
   const [search, setSearch] = useState<string>("");
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [recapData, setRecapData] = useState<SalesRecap[]>(salesRecap);
+  const [saleRecapId, setSaleRecapId] = useState<string | null>();
+  const router = useRouter();
 
   const recapDataWithPeriod = recapData.map((item) => {
     const date = new Date(item.year, item.month - 1); // -1 karena bulan mulai dari 0
@@ -61,8 +65,6 @@ export default function RekapPenjualanView({
   };
 
   const handleAddSalesRecap = async (period: string) => {
-    console.log(period);
-
     try {
       const formData = new FormData();
       formData.append("period", period);
@@ -78,6 +80,11 @@ export default function RekapPenjualanView({
         toast.error(req.message || "Gagal menambahkan rekapitulasi penjualan");
         return;
       }
+
+      const saleRecapId = req.payload.id.id;
+
+      setSaleRecapId(saleRecapId);
+      setIsModalOpen(true);
 
       toast.success(
         req.message || "Berhasil menambahkan rekapitulasi penjualan"
@@ -116,6 +123,31 @@ export default function RekapPenjualanView({
         onCloseAction={() => setShowModal(false)}
         onSubmitAction={handleAddSalesRecap}
       />
+
+      {/* Modal setelah forecasting berhasil */}
+      <Modal
+        isOpen={isModalOpen}
+        onCloseAction={() => setIsModalOpen(false)}
+        title="Ingin melihat hasil rekapitulasi penjualan?"
+      >
+        <div className="flex flex-col gap-4 items-center">
+          <p className="text-center">
+            Rekapitulasi penjualan berhasil dibuat. Klik tombol di bawah untuk
+            melihat hasilnya.
+          </p>
+          <button
+            onClick={() => {
+              setIsModalOpen(false);
+              if (saleRecapId) {
+                router.push(`/dashboard/rekap-penjualan/${saleRecapId}`);
+              }
+            }}
+            className="px-4 py-2 bg-primary text-white rounded-md"
+          >
+            Lihat
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 }
