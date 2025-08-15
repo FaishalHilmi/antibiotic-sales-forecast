8cb365e9367fd3505de6b7f0b6b400dec2d0a945
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-15 23:14:45 +0700

    feat: consume data of detail medicine from API

--- app/dashboard/obat/[id]/DetailObatView.tsx
+++ app/dashboard/obat/[id]/DetailObatView.tsx
@@ -1,46 +1,82 @@
 "use client";
 
-import { dummyObatDetail } from "@/data/obat";
 import ObatCard from "./components/ObatCard";
 import RiwayatStok from "./components/RiwayatStok";
 import ModalTambahStok from "./components/ModalTambahStok";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { MedicineProps } from "@/types/medicine";
+import toast from "react-hot-toast";
 
-export default function DetailObatView() {
+export default function DetailObatView({ medicines }: MedicineProps) {
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [riwayatStok, setRiwayatStok] = useState([
-    { id: 1, jumlah: 50, keterangan: "Stok awal masuk" },
-    { id: 2, jumlah: 30, keterangan: "Restok bulan Juli" },
-  ]);
+  const [stockHistory, setStockHistory] = useState([]);
 
-  const handleTambahStok = (jumlah: number, keterangan: string) => {
-    const newRiwayat = {
-      id: riwayatStok.length + 1,
-      jumlah,
-      keterangan,
-    };
+  const getStocskHistory = async () => {
+    try {
+      const res = await fetch(`/api/medicines/${medicines.id}/stock`, {
+        method: "GET",
+      });
 
-    setRiwayatStok([newRiwayat, ...riwayatStok]);
-    setShowModal(false);
+      const req = await res.json();
+      const stockHistory = req.payload;
+      setStockHistory(stockHistory);
+    } catch (error: any) {
+      toast.error(error.message);
+    }
   };
+
+  const handleAddStock = async (
+    quantity: number,
+    action: string,
+    note: string
+  ) => {
+    try {
+      const formData = new FormData();
+      formData.append("quantity", quantity.toString());
+      formData.append("action", action);
+      formData.append("note", note);
+
+      const res = await fetch(`/api/medicines/${medicines.id}/stock`, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!res.ok) {
+        const err = await res.json();
+        throw new Error(err.message || "Gagal menambah stok");
+      }
+
+      const data = await res.json();
+      toast.success(data.message || "Stok berhasil diperbarui");
+
+      await getStocskHistory();
+      setShowModal(false);
+    } catch (error: any) {
+      toast.error(error.message || "Terjadi kesalahan saat menambah stok");
+    }
+  };
+
+  useEffect(() => {
+    getStocskHistory();
+  }, []);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
       {/* Detail Obat */}
       <div className="col-span-1 md:col-span-2">
-        <ObatCard obat={dummyObatDetail} />
+        <ObatCard medicines={medicines} />
       </div>
       {/* Riwayat Stok */}
       <div className="col-span-1">
         <RiwayatStok
-          riwayatStok={riwayatStok}
+          stockHistory={stockHistory}
           setShowModaTambah={() => setShowModal(true)}
         />
       </div>
       <ModalTambahStok
         isOpen={showModal}
         onCloseAction={() => setShowModal(false)}
-        onSubmitAction={handleTambahStok}
+        onSubmitAction={handleAddStock}
       />
     </div>
   );
