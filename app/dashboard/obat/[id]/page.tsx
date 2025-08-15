2ba0b2be0d2e90b0c2ca4a4fc95d83ae5d988f19
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-15 23:14:23 +0700

    feat: fetching API for data of detail medicine

--- app/dashboard/obat/[id]/page.tsx
+++ app/dashboard/obat/[id]/page.tsx
@@ -1,7 +1,33 @@
 import Link from "next/link";
 import DetailObatView from "./DetailObatView";
+import { cookies } from "next/headers";
 
-export default function DetailObatPage() {
+export default async function DetailObatPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const cookieStore = await cookies();
+  const { id: medicineId } = await params;
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/medicines/${medicineId}`,
+    {
+      method: "GET",
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw Error("Gagal fetch API");
+  }
+
+  const req = await res.json();
+  const medicine = req.payload;
+
   return (
     <div className="wrapper">
       <h2 className="text-2xl font-bold mb-1">Detail Obat</h2>
@@ -14,13 +40,13 @@ export default function DetailObatPage() {
           Kembali
         </Link>
         <Link
-          href={"/dashboard/obat/edit/1"}
+          href={`/dashboard/obat/edit/${medicineId}`}
           className="px-4 py-2 bg-primary text-white rounded-md"
         >
           Ubah Obat
         </Link>
       </div>
-      <DetailObatView />
+      <DetailObatView medicines={medicine} />
     </div>
   );
 }
