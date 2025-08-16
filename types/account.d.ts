364783c5828a3e54187b3d27e6301d6b80a0a628
Faisal Hilmi <faisalhilmi082@gmail.com>
2025-08-16 22:48:44 +0700

    add: cashier account type identify

---
+++ types/account.d.ts
@@ -0,0 +1,11 @@
+export interface cashierAccount {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+  role: string;
+  imagePath: string | null;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+}
